1b78538219b274cdeb2e5d41435d08777dd9d935
Yifeng Wang <i@ewind.us>
2021-10-06 16:25:47 +0800

    fix: compiler warning

--- libquickjs-sys/embed/quickjs/patch.h
+++ libquickjs-sys/embed/quickjs/patch.h
@@ -1,8 +1,15 @@
 #pragma once
 
+#include <inttypes.h>
+#include <stdarg.h>
+
 int printf(const char *format, ...);
 
-int vsnprintf(char *__restrict, size_t, const char *__restrict, __VALIST);
+int sprintf(char *str, const char *format, ...);
+
+int snprintf(char *str, size_t size, const char *format, ...);
+
+int vsnprintf(char *str, size_t size, const char *format, va_list ap);
 
 #define putchar(str) (0)
 
